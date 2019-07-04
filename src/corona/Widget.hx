a62ab11ae455b5ff26100f91a830bf419a7567a7
e.blagodarev <ze0nni@gmail.com>
2019-07-04 13:10:19 +0300

    Add support for 9Slice buttons

--- src/corona/Widget.hx
+++ src/corona/Widget.hx
@@ -39,20 +39,40 @@ abstract LabelColor(Dynamic) {
 	}
 }
 
-typedef ButtonOptions = {
+abstract ButtonOptions(Dynamic) {
+	inline
+	function new(source: Dynamic) {
+		this = source;
+	}
+	
+	@:from
+	static
+	inline
+	public function fromFiles(source: ButtonOptionsFromFile) {
+		return new ButtonOptions(source);
+	}
+	
+	@:from
+	static
+	inline
+	public function fromSheet (source: ButtonOptionsFromSheet) {
+		return new ButtonOptions(source);
+	}
+	
+	@:from
+	static
+	inline
+	public function from9Slice(source: ButtonOptionsFrom9Slice) {
+		return new ButtonOptions(source);
+	}
+}
+
+private typedef ButtonOptionsBase = {
 	?id: String,
-	?left: Float, ?top: Float,
+	?left: Float,
+	?top: Float,
 	?width: Float,
 	?height: Float,
-	
-	?sheet: ImageSheet,
-	?defaultFrame: AtlasIndex,
-	?overFrame: AtlasIndex,
-	
-	?baseDir: DirectoryConstant,
-	?defaultFile: String,
-    ?overFile: String,
-
 	
 	?isEnabled: Bool,
 	?onPress: Void -> Void,
@@ -63,6 +83,46 @@ typedef ButtonOptions = {
 	?label: String,
 	
 	?labelColor: LabelColor
+}
+
+typedef ButtonOptionsFromFile = {
+	> ButtonOptionsBase,
+	
+	baseDir: DirectoryConstant,
+	defaultFile: String,
+    ?overFile: String
+}
+
+typedef ButtonOptionsFromSheet = {
+	> ButtonOptionsBase,
+	
+	sheet: ImageSheet,
+	defaultFrame: AtlasIndex,
+	?overFrame: AtlasIndex
+}
+
+typedef ButtonOptionsFrom9Slice = {
+	> ButtonOptionsBase,
+	
+	sheet: ImageSheet,
+	topLeftFrame: AtlasIndex,
+	topMiddleFrame: AtlasIndex,
+	topRightFrame: AtlasIndex,
+	middleLeftFrame: AtlasIndex,
+	middleFrame: AtlasIndex,
+	middleRightFrame: AtlasIndex,
+	bottomLeftFrame: AtlasIndex,
+	bottomMiddleFrame: AtlasIndex,
+	bottomRightFrame: AtlasIndex,
+	topLeftOverFrame: AtlasIndex,
+	topMiddleOverFrame: AtlasIndex,
+	topRightOverFrame: AtlasIndex,
+	middleLeftOverFrame: AtlasIndex,
+	middleOverFrame: AtlasIndex,
+	middleRightOverFrame: AtlasIndex,
+	bottomLeftOverFrame: AtlasIndex,
+	bottomMiddleOverFrame: AtlasIndex,
+	bottomRightOverFrame: AtlasIndex
 }
 
 extern class ButtonWidget extends DisplayObject {
