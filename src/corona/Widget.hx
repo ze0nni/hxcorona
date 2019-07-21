0ebd1cf072160fc0054ad5888f61ff80e1221682
ze0nni <ze0nni@gmail.com>
2019-07-21 10:38:14 +0300

    Append widget.newScrollView

--- src/corona/Widget.hx
+++ src/corona/Widget.hx
@@ -1,11 +1,14 @@
 package corona;
+import corona.Display.Coordinates;
 import corona.Display.DisplayObject;
 import corona.Display.GroupObject;
 import corona.Display.ShapeObject;
 import corona.Display.ColorPaint;
+import corona.Display.TouchEvent;
 import corona.Graphics.AtlasIndex;
 import corona.Graphics.ImageSheet;
 import corona.System.DirectoryConstant;
+import corona.Transition.Callback;
 
 /**
  * ...
@@ -18,7 +21,7 @@ extern class Widget
 	static function newButton(options: ButtonOptions): ButtonWidget;
 	//widget.newPickerWheel()
 	//widget.newProgressView()
-	//widget.newScrollView()
+	static function newScrollView(options: ScrollViewOptions): ScrollView;
 	//widget.newSegmentedControl()
 	//widget.newSlider()
 	//widget.newSpinner()
@@ -131,5 +134,90 @@ extern class ButtonWidget extends DisplayObject {
 	function setEnabled(value: Bool): Void;
 	//object:setFillColor()
 	//object:setStrokeColor()
+
+}
+
+@:enum abstract ScrollViewAxis(String) {
+	var horizontal = "horizontal";
+	var vertical = "vertical";
+}
+
+typedef ScrollViewOptions = {
+	?id: String,
+	?x: Float,
+	?y: Float,
+	?left: Float,
+	?top: Float,
+	?width: Float,
+	?height: Float,
+	?scrollWidth: Float,
+	?scrollHeight: Float,
+	?friction: Float,
+	?horizontalScrollDisabled: Bool,
+	?verticalScrollDisabled: Bool,
+	?isLocked: Bool,
+	?isBounceEnabled: Bool,
+	?listener: ScrollViewListener
+}
+
+typedef ScrollBarOptions = {
+	sheet: ImageSheet,
+	topFrame: AtlasIndex,
+	middleFrame: AtlasIndex,
+	bottomFrame: AtlasIndex
+}
+
+abstract ScrollViewListener(TouchEvent -> Bool) from (TouchEvent -> Bool) {
+	
+}
+
+typedef ScrollToPositionOptions = {
+	?x: Float,
+	?y: Float,
+	?time: Float,
+	?onComplete: Callback
+}
+
+@:enum abstract ScrollToPosition(String) {
+	var top = "top";
+	var bottom = "bottom";
+	var left = "left";
+	var right = "right";
+}
+
+typedef ScrollToOptions = {
+	?time: Float,
+	?onComplete: Callback	
+}
+
+/**
+ * ScrollView may be GroupObject but I have a bug:
+ * 
+ * 		var scroll: ScrollView = ...;
+ * 		Display.NewRectIn(scroll, 0, 0, 50, 50);
+ * 
+ * In this case Shape added to ScrollView incorrect
+ */
+extern class ScrollView extends DisplayObject {
+	var backgroundColor(default, default): ColorPaint;
+	var hideBackground(default, default): Bool;
+	var hideScrollBar(default, default): Bool;
+	var scrollBarOptions(default, default): ScrollBarOptions;
+	var topPadding(default, default): Float;
+	var bottomPadding(default, default): Float;
+	var leftPadding(default, default): Float;
+	var rightPadding(default, default): Float;
+	
+	function insert(child: DisplayObject, ?resetTransform: Bool): Void;
+	function remove(index: Int): DisplayObject;
+	
+	function getContentPosition(): Coordinates;
+	function getView(): GroupObject;
+	function scrollToPosition(options: ScrollToPositionOptions): Void;
+	function scrollTo(position: ScrollToPosition, options: ScrollToOptions): Void;
+	function setIsLocked(isLocked: Bool, ?axis: ScrollViewAxis): Void;
+	function setScrollWidth(newWidth: Float): Float;
+	function setScrollHeight(newHeight: Float): Float;
+	function takeFocus(event: TouchEvent): Float;
 
 }
