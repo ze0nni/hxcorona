3b5b3386ff32acffd266f2eb92c63168a52b9ff3
e.blagodarev <ze0nni@gmail.com>
2019-10-26 15:23:03 +0300

    add 'LuaProducer', 'LuaConsumer', 'LuaBiConsumer' and 'LuaBiFunction' abstracts

--- src/corona/LuaFunctions.hx
+++ src/corona/LuaFunctions.hx
@@ -1,7 +1,31 @@
 package corona;
 
+abstract LuaProducer<A>(Void -> A) from (Void -> A) {
+	inline public function call(): A {
+		return this();
+	}
+}
+
+abstract LuaConsumer<A>(A -> Void) from (A -> Void) {
+	inline public function call(a: A): Void {
+		return this(a);
+	}
+}
+
+abstract LuaBiConsumer<A,B>(A -> B -> Void) from (A -> B -> Void) {
+	inline public function call(a: A, b: B): Void {
+		return this(a, b);
+	}
+}
+
 abstract LuaFunction<A,B>(A -> B) from (A -> B) {
 	inline public function call(a: A): B {
 		return this(a);
+	}
+}
+
+abstract LuaBiFunction<A,B,C>(A -> B -> C) from (A -> B -> C) {
+	inline public function call(a: A, b: B): C {
+		return this(a, b);
 	}
 }
